26e19793c1eebc83e48a16b78035593953bc7803
DejonGill <dejonjgill@gmail.com>
2016-11-26 15:15:45 -0800

    complete

--- ds/Stack/exercises/palindrome.js
+++ ds/Stack/exercises/palindrome.js
@@ -1,3 +1,18 @@
 module.exports = (str) => {
-  return false;
+  if (typeof str !== 'string') {
+    return false;
+  }
+
+  const Stack = require('../stack_adt');
+  const s = new Stack();
+
+  str.split('').forEach((letter) => {
+    s.push(letter);
+  });
+  var res = '';
+  while (s.size > 0) {
+    res += s.pop();
+  }
+
+  return res === str;
 };
