d8e37a9a6c0a309a3c89fbb3eb6c2a8076f80dbb
DejonGill <dejonjgill@gmail.com>
2016-11-26 14:24:17 -0800

    Tests for exercise one complete

--- spec/stack/numToBase_tests.js
+++ spec/stack/numToBase_tests.js
@@ -10,6 +10,34 @@ describe('numToBase', function() {
   });
   it('should convert a number to a base 2 string', function() {
     let n = 42;
-    expect(n.toString(2)).to.equal('101010');
+    expect(numToBase(2, 2)).to.equal(n.toString(2));
+  });
+  it('should convert a number to a base 3 string', function() {
+    let n = 42;
+    expect(numToBase(42, 3)).to.equal(n.toString(3));
+  });
+  it('should convert a number to a base 4 string', function() {
+    let n = 42;
+    expect(numToBase(42, 4)).to.equal(n.toString(4));
+  });
+  it('should convert a number to a base 5 string', function() {
+    let n = 42;
+    expect(numToBase(42, 5)).to.equal(n.toString(5));
+  });
+  it('should convert a number to a base 6 string', function() {
+    let n = 42;
+    expect(numToBase(42, 6)).to.equal(n.toString(6));
+  });
+  it('should convert a number to a base 7 string', function() {
+    let n = 42;
+    expect(numToBase(42, 7)).to.equal(n.toString(7));
+  });
+  it('should convert a number to a base 8 string', function() {
+    let n = 42;
+    expect(numToBase(42, 8)).to.equal(n.toString(8));
+  });
+  it('should convert a number to a base 9 string', function() {
+    let n = 42;
+    expect(numToBase(42, 9)).to.equal(n.toString(9));
   });
 })
