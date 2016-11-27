36b162b873bf38a3119a43a9cee8549ef53d6ab9
DejonGill <dejonjgill@gmail.com>
2016-11-26 20:32:08 -0800

    Complete

--- ds/Stack/exercises/balancedParens.js
+++ ds/Stack/exercises/balancedParens.js
@@ -1,5 +1,32 @@
 module.exports = function(str) {
+  const Stack = require('../stack_adt');
+
   if (typeof str !== 'string') {
     return new Error('Input must be a string');
   }
+  if (str.length == 0) { return true; }
+
+  const openingBrackets = new Stack();
+
+  let braces = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
+  };
+  let currentBrace;
+
+  for (let i = 0; i < str.length; i++) {
+    currentBrace = str[i];
+    if (currentBrace in braces) {
+      openingBrackets.push(currentBrace);
+      continue;
+    }
+    if (currentBrace.search(/(})|(\))|(\])+/) !== -1) {
+      if (openingBrackets.size === 0 || braces[openingBrackets.pop()] !== currentBrace) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
