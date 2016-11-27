a046f901dc31c897b681266ed27f640ceb44dd3d
DejonGill <dejonjgill@gmail.com>
2016-11-26 16:05:46 -0800

    Tests for proper argument data type

--- spec/stack/balancedParens_tests.js
+++ spec/stack/balancedParens_tests.js
@@ -6,4 +6,13 @@ describe('balancedParens', function() {
     expect(balancedParens instanceof Function).to.equal(true);
   });
 
+  it('should return an error if the input is not a string', function() {
+    expect(balancedParens(123) instanceof Error).to.equal(true);
+    expect(balancedParens([1, 2, 3, 'a']) instanceof Error).to.equal(true);
+    expect(balancedParens({}) instanceof Error).to.equal(true);
+    expect(balancedParens(true) instanceof Error).to.equal(true);
+    expect(balancedParens(null) instanceof Error).to.equal(true);
+    expect(balancedParens() instanceof Error).to.equal(true);
+  });
+
 });
