2e40c1f07d713fefaa8c23eb34103adb1180cb05
DejonGill <dejonjgill@gmail.com>
2016-11-26 21:44:44 -0800

    renamed test files for clarity, started tests for pezDispenser

---
+++ spec/stack/pezDispenser.test.js
@@ -0,0 +1,12 @@
+describe('pezDispenser', function() {
+  const { expect } = require('chai');
+  const pezDispenser = require('../../ds/Stack/exercises/pezDispenser');
+
+  describe('structure and methods', function() {
+    it('should be a function', function() {
+      expect(pezDispenser instanceof Function).to.equal(true);
+    });
+
+
+  });
+});
