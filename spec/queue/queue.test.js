272c4d5b7950be28e8a8428f23cab9718546de8a
DejonGill <dejonjgill@gmail.com>
2016-11-27 13:19:27 -0800

    initial constructor unit tests complete

--- spec/queue/queue.test.js
+++ spec/queue/queue.test.js
@@ -2,9 +2,26 @@ describe('Queue', function() {
   const { expect } = require('chai');
   const Queue = require('../../ds/Queue/queue_adt');
   describe('structure', function() {
+    let q = new Queue();
+
     it('should be a function', function (done) {
-      let q = new Queue();
-      expect(typeof q === 'function').to.equal(true);
+      expect(typeof Queue === 'function').to.equal(true);
+    });
+
+    it('should use an object for storage', function (done) {
+      expect(typeof q.storage === 'object').to.equal(true);
+    });
+
+    it('should have a property size, which is of type number', function() {
+      expect(typeof q.size).to.equal('number');
+    });
+
+    it('should have a method called enqueue', function (done) {
+      expect(q.enqueue instanceof Function).to.equal(true);
+    });
+
+    it('should have a method called dequeue', function (done) {
+      expect(q.dequeue instanceof Function).to.equal(true);
     });
   });
 });
