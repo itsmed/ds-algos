4db068d174ce9a02b97cc3704f06744658b4807a
DejonGill <dejonjgill@gmail.com>
2016-11-30 15:28:18 -0800

    starting to implement linked list

---
+++ ds/LinkedLists/linkedList_adt.js
@@ -0,0 +1,7 @@
+class LinkedList {
+  constructor() {
+
+  }
+}
+
+module.exports = LinkedList;
--- spec/linkedLists/linkedList.test.js
+++ spec/linkedLists/linkedList.test.js
@@ -1,18 +1,29 @@
 describe('Linked List', function() {
   const { expect } = require('chai');
-  const LinkedList = require('../../ds/LinkedList/linkedList_adt');
+  const LinkedList = require('../../ds/LinkedLists/linkedList_adt');
 
   describe('constructor', function() {
-    it('should be a function', function (done) {
-      expect(LinkedList instanceof Function).to.equal(true);
+    let l = new LinkedList();
+
+    it('should be a function', function () {
+      console.log('ffsfsf', l instanceof Function);
+      expect(typeof LinkedList).to.equal('function');
     });
 
-    it('should have a createNode method', function (done) {
-      expect(LinkedList.createNode instanceof Function).to.equal(true);
+    it('should have a createNode method', function () {
+      expect(l.createNode instanceof Function).to.equal(true);
     });
 
-    it('should have an insertAfter method', function (done) {
-      expect(LinkedList.insertAfter instanceof Function).to.equal(true);
+    it('should have an insertAfter method', function () {
+      expect(l.insertAfter instanceof Function).to.equal(true);
+    });
+
+    it('should have a removeNode method', function () {
+      expect(l.removeNode instanceof Function).to.equal(true);
+    });
+
+    it('should have a displayList method', function () {
+      expect(l.displayList instanceof Function).to.equal(true);
     });
   });
 });
