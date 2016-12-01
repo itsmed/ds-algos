c2c2dff4825f183b3892815453c74b4ba783d2ed
DejonGill <dejonjgill@gmail.com>
2016-12-01 14:10:24 -0800

    modified removeNode method to work for doublyLinkedList

--- ds/LinkedLists/doublyLinkedList_adt.js
+++ ds/LinkedLists/doublyLinkedList_adt.js
@@ -12,14 +12,40 @@ class DoublyLinkedList extends LinkedList {
       previous: null
     })
   }
+
+  insertAfter(target, value) {
+    let n = this.createNode(value);
+
+    let t = this.findValue(target);
+
+    if (t === false) {
+      return new Error('target not found in list, node not added');
+    }
+
+    n.next = t.next;
+    n.previous = t;
+    t.next = n;
+  }
+
+  removeNode(target) {
+
+    let t = this.findValue(target);
+    console.log('tttttt', t);
+    if (t === false) {
+      return new Error('target not found in list, node not added');
+    } else {
+      t.next.previous = t.previous;
+      t.previous.next = t.next;
+    }
+  }
 }
 
 module.exports = DoublyLinkedList;
 
 
-var m = new DoublyLinkedList(0);
+// var m = new DoublyLinkedList(0);
 
-m.insertAfter(0, 1);
-m.insertAfter(1, 2);
+// m.insertAfter(0, 1);
+// m.insertAfter(1, 2);
 
-console.log(m);
+// console.log(m);
