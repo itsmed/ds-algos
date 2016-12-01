9ae76a703df3c1e56234b67fd18a4fe4e979b4a3
DejonGill <dejonjgill@gmail.com>
2016-12-01 13:52:48 -0800

    beginning tests for dll, extended ll using es6

--- ds/LinkedLists/doublyLinkedList_adt.js
+++ ds/LinkedLists/doublyLinkedList_adt.js
@@ -4,4 +4,20 @@ class DoublyLinkedList extends LinkedList {
   constructor(val) {
     super(val);
   }
+
+  createNode(val) {
+    return ({
+      value: val,
+      next: null,
+      previous: null
+    })
+  }
 }
+
+
+var m = new DoublyLinkedList(0);
+
+m.insertAfter(0, 1);
+m.insertAfter(1, 2);
+
+console.log(m);
